5dc4a6d7df48c9b47c00e89472a6ab5c7c9ef48b
michaelrcfj <131567050+michaelrcfj@users.noreply.github.com>
2023-04-30 16:53:41 +1000

    Add files via upload

--- WineApp.py
+++ WineApp.py
@@ -2,11 +2,13 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+from matplotlib import pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score, RocCurveDisplay, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, f1_score,confusion_matrix, ConfusionMatrixDisplay
+
 
 # from streamlit_pandas_profiling import st_profile_report
 # from ydata_profiling import profile_report
@@ -28,7 +30,7 @@ wine=wine.rename(columns={"fixed acidity": "Sourness", "citric acid": "Fruitines
                          "alcohol":"Alcohol%","quality":"Quality"})
 
 ############################# Sidebar | Quality threshold #############################
-st.sidebar.header('Wine Tasting with AI', help='A Machine 101 exercise')
+st.sidebar.header('Wine Tasting with AI', help='A Machine Learning 101 exercise')
 st.sidebar.subheader('Problem Statement')
 st.sidebar.write("""You want to impress your wine enthusiast friends by buying them fine Portuguese red wine.
 
@@ -53,7 +55,7 @@ st.divider()
 
 ############################# Step 2 - Baseline #############################
 
-st.header(':blue[Step 2 - Set a baseline]')
+st.header(':blue[Step 2 - Set a baseline]📏')
 baseline_1,baseline_2,baseline_3=st.columns(3)
 with baseline_1:
     bsline1=st.radio("Can you taste?",['Yes','No'],help="Read the problem statement")
@@ -94,6 +96,8 @@ st.divider()
 
 st.header(':blue[Step 3 - First ML Model - Decision Tree]')
 
+st.write("Find out more about [decision trees](https://mlu-explain.github.io/decision-tree/)")
+
 ###### Separate Features and Label ######
 y=wine['Quality']
 x_temp=wine.drop(['Quality'],axis=1)
@@ -105,7 +109,7 @@ st.write('Have a go at building 3 decision trees with different settings and see
 DS1, DS2, DS3 = st.columns(3)
 
 with DS1:
-    st.subheader("Tree 1")
+    st.subheader("Tree 1🌲")
     selected_features1=st.multiselect("Features",x_temp.columns,'Sourness'
                                      , help="What **inputs/features** help predict wine quality?", key="feat1")
     DS_Depth1 = st.slider('Decision Tree Max Depth', 1, 10, 5, help='How big do you want the decision tree to be?'
@@ -113,12 +117,12 @@ with DS1:
 
 
 with DS2:
-    st.subheader("Tree 2")
+    st.subheader("Tree 2🌲")
     selected_features2=st.multiselect("Features",x_temp.columns,'Sourness', key="feat2")
     DS_Depth2 = st.slider('Decision Tree Max Depth', 1, 10, 5, key="ds_dep2")
 
 with DS3:
-    st.subheader("Tree 3")
+    st.subheader("Tree 3🌲")
     selected_features3=st.multiselect("Features",x_temp.columns,'Sourness', key="feat3")
     DS_Depth3 = st.slider('Decision Tree Max Depth', 1, 10, 5, key="ds_dep3")
 
@@ -146,7 +150,7 @@ val_predict1=wine_ds1.predict(val_x1)
 val_predict2=wine_ds2.predict(val_x2)
 val_predict3=wine_ds3.predict(val_x3)
 
-model_acc_ds1=round(accuracy_score(val_y1, val_predict1)*100,0)
+model_acc_ds1=round(accuracy_score(val_y1, val_predict1)*100,2)
 model_acc_ds2=round(accuracy_score(val_y2, val_predict2)*100,2)
 model_acc_ds3=round(accuracy_score(val_y3, val_predict3)*100,2)
 
@@ -154,17 +158,65 @@ model_f1_ds1=round(f1_score(val_y1, val_predict1)*100,2)
 model_f1_ds2=round(f1_score(val_y2, val_predict2)*100,2)
 model_f1_ds3=round(f1_score(val_y3, val_predict3)*100,2)
 
+st.subheader("Split Data")
+st.write("For simplicity, we have split the data **75:25**")
+
+st.write("""**75%** of the data is used to train the model
+
+**25%** is used to test the model 
+""")
+st.write("Find out more about [Train, Validation, Testing Splits](https://mlu-explain.github.io/decision-tree/)")
+
+st.subheader("Train the Model")
+
 st.write(":blue[$◀$]Check your model scores in the **left sidebar**")
 
 st.divider()
 
+
+############################# See decision tree results #############################
+st.header(':blue[See Decision Tree Outcomes]')
+st.subheader("Based on Decision Tree 3🌲")
+
+st.caption("Hint: You can go back up and edit the settings of Tree 3")
+st.write("What has the model actually predicted?")
+
+actual_output=pd.concat((val_x3,val_y3,
+                        pd.DataFrame(val_predict3,index = val_x3.index.copy(),columns=['Predicted Quality']),
+                        pd.DataFrame(wine_ds3.predict_proba(val_x3),index = val_x3.index.copy(),columns=['a','Model Score']).drop(['a'],axis=1)
+                        ),axis=1)
+
+actual_output['Quality'] = np.where(actual_output['Quality']==1 , '1|🍷','0|💩')
+actual_output['Predicted Quality'] = np.where(actual_output['Predicted Quality']==1 , '1|🍷','0|💩')
+actual_output['Model Correct?']= np.where(actual_output['Quality']==actual_output['Predicted Quality'],"✅","❌")
+
+
+actual_output= actual_output[selected_features3+['Model Score', 'Predicted Quality','Quality','Model Correct?']]
+actual_output=actual_output.rename(columns={"Quality": "Actual Quality"})
+st.write(actual_output)
+st.divider()
+
+############################# Confusion Matrix #############################
+st.header(':blue[Confusion Matrix]')
+
+cm = confusion_matrix(val_y3, val_predict3)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+
+
+disp.plot()
+st.pyplot()
+
+st.divider()
 ############################# Random Forest #############################
 st.header(':blue[Step 4 - Random Forest]')
+
+st.write("Find out more about [Random Forest](https://mlu-explain.github.io/random-forest/)")
+
 x_list=x_temp.columns.to_list()
 x_list=", ".join(map(str,x_list))
 st.write("Using **all features**: ",x_list)
 
-RF_Dep = st.slider('How many trees do you want in this forest?', 100, 300, 250, key="RF_D")
+RF_Dep = st.slider('How many trees do you want in this forest?', 1, 250, 150, key="RF_D")
 
 x4=wine.drop(['Quality'],axis=1)
 train_x4, val_x4, train_y4, val_y4 = train_test_split(x4, y, random_state=rand_seed)
@@ -175,12 +227,12 @@ wine_rf.fit(train_x4, train_y4)
 ###### Model prediction and scoring ######
 val_predict4=wine_rf.predict(val_x4)
 
-model_acc_rf=round(accuracy_score(val_y4, val_predict4)*100,0)
+model_acc_rf=round(accuracy_score(val_y4, val_predict4)*100,2)
 model_f1_rf=round(f1_score(val_y4, val_predict4)*100,2)
-
 
 ############################# Model Scoring #############################
 st.sidebar.subheader("Model Accuracy %")
+st.sidebar.write("*Adjust settings to the right* :blue[▶]")
 st.sidebar.write("Basline 🙈: ",50.0)
 st.sidebar.write('Decision Tree 1🌲', model_acc_ds1)
 st.sidebar.write('Decision Tree 2🌲', model_acc_ds2)
